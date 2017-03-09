c12d14d77483808d6946152be11951fdb6fd7388
Igor <zolotov.igor@gmail.com>
2017-03-09 17:51:23 +0300

    implement AplusB and maxPairWiseProduct from week1 lesson

--- course1/week1/AplusB/src/AplusB.java
+++ course1/week1/AplusB/src/AplusB.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
-class APlusB {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int a = s.nextInt();
-        int b = s.nextInt();
-        System.out.println(a + b);
-    }
+public class AplusB {
+
+        public static void main(String[] args) {
+            Scanner s = new Scanner(System.in);
+            int a = s.nextInt();
+            int b = s.nextInt();
+            System.out.println(a + b);
+        }
 }
