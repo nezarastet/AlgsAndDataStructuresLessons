d66866d212a8fc0bcd76a7b1becd916259e98e18
Igor <zolotov.igor@gmail.com>
2017-03-11 17:01:01 +0300

    done MaxPairwiseProductFast

--- src/MaxPairwiseProduct.java
+++ src/MaxPairwiseProduct.java
@@ -16,6 +16,38 @@ public class MaxPairwiseProduct {
         return result;
     }
 
+    static long getMaxPairwiseProductFast(long[] numbers) {
+        long result = 0;
+        int n = numbers.length;
+
+        int idxMAxValue=0;
+        long maxVal1=-1;
+        long maxVal2=-1;
+
+// find max value in numbers
+        for (int i = 0; i < n; ++i) {
+            if(numbers[i]>maxVal1){
+                maxVal1 = numbers[i];
+                idxMAxValue=i;
+            }
+
+        }
+
+// find max value  in nubmers exept maxVal1
+        for (int i = 0; i < n; ++i) {
+            if(numbers[i]>maxVal2 && i!=idxMAxValue){
+                maxVal2 = numbers[i];
+            }
+
+        }
+
+        result=maxVal1*maxVal2;
+
+        return result;
+    }
+
+
+
     public static void main(String[] args) {
         FastScanner scanner = new FastScanner(System.in);
         int n = scanner.nextInt();
@@ -23,7 +55,10 @@ public class MaxPairwiseProduct {
         for (int i = 0; i < n; i++) {
             numbers[i] = scanner.nextInt();
         }
-        System.out.println(getMaxPairwiseProduct(numbers));
+
+//for testing        numbers = new long[200000];
+//for testing        System.out.println(getMaxPairwiseProduct(numbers) + "\n");
+        System.out.println(getMaxPairwiseProductFast(numbers));
     }
 
     static class FastScanner {
