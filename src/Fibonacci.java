18d18ad7c42cca533fedc43d5a8a4575aa755773
Igor <zolotov.igor@gmail.com>
2017-03-16 23:26:06 +0300

    done Fast Fibonacci algorithm

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -8,10 +8,35 @@ public class Fibonacci {
     return calc_fib(n - 1) + calc_fib(n - 2);
   }
 
+  private static long calc_fibFast(int n, long[] M) {
+    if (n <= 1)
+    {
+      M[n]=n;
+      return n;
+    }
+
+
+    if (M[n]<0)
+    {
+      M[n]=calc_fibFast(n - 1, M) + calc_fibFast(n - 2, M);
+      return M[n];
+    }
+    else
+    {
+      return M[n]; // calculated yet
+    }
+  }
+
   public static void main(String args[]) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
+    long[] M = new long[n+1];
+    for (int i=0; i<=n; i++)
+    {
+      M[i]=-1;
+    }
 
-    System.out.println(calc_fib(n));
+//    System.out.println(calc_fib(n));
+      System.out.println(calc_fibFast(n, M));
   }
 }
