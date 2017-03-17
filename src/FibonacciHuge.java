dd36f4f9fbc80555f3e505d1dd3ac4c287961b8e
Igor <zolotov.igor@gmail.com>
2017-03-17 23:59:12 +0300

    done LCM and FibonacciHuge

--- src/FibonacciHuge.java
+++ src/FibonacciHuge.java
@@ -16,12 +16,48 @@ public class FibonacciHuge {
 
         return current % m;
     }
+
+    private static long getFibonacciHugeFast(long n, long m) {
+// determine period pisano
+        if(n == 1) return 1;
+
+        long previous = 0;
+        long current  = 1;
+        long period = 1;
+
+        for (long i = 2; i <= n; i++) {
+            long tmp_previous = previous;
+            previous = current;
+            current = (tmp_previous + current) % m;
+
+            if (current == 1 && previous == 0) break;
+            else {
+                period++;
+            }
+
+            if (i == n) return current;
+        }
+
+        n = n % period;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        previous = 0;
+        current  = 1;
+        for (long i = 2; i <= n; i++) {
+            long tmp_previous = previous;
+            previous = current;
+            current = (tmp_previous + current) % m;
+        }
+        return current;
+    }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
         long m = scanner.nextLong();
-        System.out.println(getFibonacciHugeNaive(n, m));
+//        System.out.println(getFibonacciHugeNaive(n, m));
+        System.out.println(getFibonacciHugeFast(n, m));
     }
 }
 
--- src/LCM.java
+++ src/LCM.java
@@ -9,11 +9,32 @@ public class LCM {
     return (long) a * b;
   }
 
+  private static long lcm_fast(int a, int b) {
+   long gcd = gcd_fast(a, b);
+   return (long) a*b/gcd;
+  }
+
   public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
     int a = scanner.nextInt();
     int b = scanner.nextInt();
 
-    System.out.println(lcm_naive(a, b));
+//    System.out.println(lcm_naive(a, b));
+    System.out.println(lcm_fast(a, b));
   }
+
+  private static int gcd_fast(int a, int b) {
+    int MaxVal = Math.max(a,b);
+    int MinVal = Math.min(a, b);
+
+    if(MaxVal % MinVal ==0){
+      return MinVal;
+    }
+    else{
+      MaxVal = MaxVal % MinVal;
+      return gcd_fast( MaxVal  ,MinVal);
+    }
+
+  }
+
 }
