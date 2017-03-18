cae780e29621410d93f725de40dc403d80b95049
Igor <zolotov.igor@gmail.com>
2017-03-18 14:23:23 +0300

    done FibonacciSumLastDigit and FibonacciPartialSum. Complete Assignment 1

--- src/FibonacciPartialSum.java
+++ src/FibonacciPartialSum.java
@@ -30,7 +30,72 @@ public class FibonacciPartialSum {
         Scanner scanner = new Scanner(System.in);
         long from = scanner.nextLong();
         long to = scanner.nextLong();
-        System.out.println(getFibonacciPartialSumNaive(from, to));
+//        System.out.println(getFibonacciPartialSumNaive(from, to));
+        System.out.println(getFibonacciPartialSumFast(from, to));
     }
+
+    private static long getFibonacciPartialSumFast(long from, long to){
+       long answ =0;
+
+       if (from ==0) answ = getFibonacciSumFast(to);
+       else answ = getFibonacciSumFast(to) - getFibonacciSumFast(from-1);
+
+       if (answ < 0) answ=10+answ;
+       return (answ);
+    }
+
+    private static long getFibonacciSumNaive(long n) {
+        if (n <= 1)
+            return n;
+
+        long previous = 0;
+        long current  = 1;
+        long sum      = 1;
+
+        for (long i = 0; i < n - 1; ++i) {
+            long tmp_previous = previous;
+            previous = current;
+            current = tmp_previous + current;
+            sum += current;
+        }
+
+        return sum % 10;
+    }
+
+    private static long getFibonacciSumFast(long n) {
+        if (n <= 1)
+            return n;
+        long period = getPeriodFibModuloM (10); // calculate period for deviding by 10
+        if (n < period){
+            return (getFibonacciSumNaive(n));
+        }
+        else{
+            long periodsCount = n/period;
+            long addCount = n % period;
+            return (periodsCount * getFibonacciSumNaive(period) + getFibonacciSumNaive(addCount));
+        }
+
+
+    }
+
+    private static long getPeriodFibModuloM (long m){
+        long previous = 0;
+        long current  = 1;
+        long period = 0;
+
+        while (true) {
+            period++;
+
+            long tmp_previous = previous;
+            previous = current;
+            current = (tmp_previous + current) % m;
+
+            if (current == 1 && previous == 0) break;
+        }
+        return period;
+    }
+
+
+
 }
 
--- src/FibonacciSumLastDigit.java
+++ src/FibonacciSumLastDigit.java
@@ -18,11 +18,47 @@ public class FibonacciSumLastDigit {
 
         return sum % 10;
     }
-    
+
+    private static long getFibonacciSumFast(long n) {
+        if (n <= 1)
+            return n;
+        long period = getPeriodFibModuloM (10); // calculate period for deviding by 10
+        if (n < period){
+            return (getFibonacciSumNaive(n));
+        }
+        else{
+            long periodsCount = n/period;
+            long addCount = n % period;
+            return (periodsCount * getFibonacciSumNaive(period) + getFibonacciSumNaive(addCount));
+        }
+
+
+    }
+
+    private static long getPeriodFibModuloM (long m){
+        long previous = 0;
+        long current  = 1;
+        long period = 0;
+
+        while (true) {
+            period++;
+
+            long tmp_previous = previous;
+            previous = current;
+            current = (tmp_previous + current) % m;
+
+            if (current == 1 && previous == 0) break;
+        }
+        return period;
+    }
+
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
-        long s = getFibonacciSumNaive(n);
+//        long s = getFibonacciSumNaive(n);
+          long s = getFibonacciSumFast(n);
         System.out.println(s);
     }
 }
