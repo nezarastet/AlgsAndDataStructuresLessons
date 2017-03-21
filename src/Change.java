d1d5ecff2a80d0227faefef4ec336aeff038053e
Igor <zolotov.igor@gmail.com>
2017-03-21 23:23:33 +0300

    done Change task

--- src/Change.java
+++ src/Change.java
@@ -1,15 +1,28 @@
 import java.util.Scanner;
 
 public class Change {
-    private static int getChange(int m) {
-        //write your code here
-        return m;
+    private static int getChange(int m, int[] coinDenominations, int coinCount) {
+        for(int i = 0; i < coinDenominations.length; i++){
+            if( m >= coinDenominations[i]){
+                coinCount = coinCount + m/coinDenominations[i];
+                m = m % coinDenominations[i];
+                getChange(m, coinDenominations, coinCount);
+            }
+        }
+        return coinCount;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int m = scanner.nextInt();
-        System.out.println(getChange(m));
+        int [] coinDenominations = new int[3];
+        coinDenominations[0]=10;
+        coinDenominations[1]=5;
+        coinDenominations[2]=1;
+        int coinCount = 0;
+
+        System.out.println(getChange(m, coinDenominations, coinCount));
+
 
     }
 }
