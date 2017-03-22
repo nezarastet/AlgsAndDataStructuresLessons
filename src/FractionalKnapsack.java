4122e22b2103ac24043c6b18ce850c8dcf104cdb
Igor <zolotov.igor@gmail.com>
2017-03-22 23:46:23 +0300

    done FractionalKnapsack task from homework week 3

--- src/FractionalKnapsack.java
+++ src/FractionalKnapsack.java
@@ -1,9 +1,33 @@
 import java.util.Scanner;
 
 public class FractionalKnapsack {
-    private static double getOptimalValue(int capacity, int[] values, int[] weights) {
-        double value = 0;
-        //write your code here
+    private static double getOptimalValue(int capacity, int[] values, int[] weights, double value) {
+
+        if(capacity ==0) return value;
+        int valuesSum = 0;
+        for( int num : values) {
+            valuesSum = valuesSum+num;
+        }
+        if(valuesSum == 0) return value;
+        else{
+            double minWeightPerUnit = 10e7;
+            int idx=-1;
+            for (int i =0; i < values.length; i++){
+                if(values[i] != 0 && (double)weights[i]/(double)values[i] < minWeightPerUnit){
+                    minWeightPerUnit = (double)weights[i]/(double)values[i];
+                    idx = i;
+                }
+            }
+            if(capacity >= weights[idx]){
+                capacity = capacity - weights[idx];
+                value = value + values[idx];
+                values[idx] = 0;
+                value = getOptimalValue(capacity, values, weights,value);
+            }
+            else{
+                value = value +  ((double)values[idx]/(double)weights[idx]) * (double)capacity;
+            }
+        }
 
         return value;
     }
@@ -18,6 +42,7 @@ public class FractionalKnapsack {
             values[i] = scanner.nextInt();
             weights[i] = scanner.nextInt();
         }
-        System.out.println(getOptimalValue(capacity, values, weights));
+        double value = 0;
+        System.out.println(getOptimalValue(capacity, values, weights,value));
     }
 } 
