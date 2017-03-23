b1b81e07f68d636461212c6ccbe34438569b68d2
Igor <zolotov.igor@gmail.com>
2017-03-23 23:46:45 +0300

    write code for DotProduct and CoveringSegments. not pass

--- src/CoveringSegments.java
+++ src/CoveringSegments.java
@@ -4,22 +4,57 @@ public class CoveringSegments {
 
     private static int[] optimalPoints(Segment[] segments) {
         //write your code here
-        int[] points = new int[2 * segments.length];
-        for (int i = 0; i < segments.length; i++) {
-            points[2 * i] = segments[i].start;
-            points[2 * i + 1] = segments[i].end;
-        }
+//        int[] points = new int[2 * segments.length];
+//        for (int i = 0; i < segments.length; i++) {
+//            points[2 * i] = segments[i].start;
+//            points[2 * i + 1] = segments[i].end;
+//        }
+        int[] points = new int[0];
+        points = calcOptimalPoints(segments, points);
         return points;
+    }
+
+    public static int[] calcOptimalPoints (Segment[] segments, int[] points){
+        int countNotVisitedSegments = 0;
+        for(int i = 0; i < segments.length; i++){
+            if (segments[i].visited == false) countNotVisitedSegments = countNotVisitedSegments+1;
+        }
+        if (countNotVisitedSegments == 0) return points;
+        else {
+            int[] points_new = new int[points.length + 1]; // add one point
+            System.arraycopy(points, 0, points_new, 0, points.length);
+
+
+            int minRightEnd = 1000000; // !!!!!!!!!!!!!!!!!!!!
+            int idx_minRightEnd = -1;
+             for (int i = 0; i <segments.length; i++){
+                 if (segments[i].end < minRightEnd && segments[i].visited == false){
+                     minRightEnd = segments[i].end;
+                     idx_minRightEnd = i;
+                 }
+            }
+            for (int i = 0; i<segments.length;i++){
+                 if (segments[i].start <= minRightEnd && segments[i].end >= minRightEnd) segments[i].visited=true;
+            }
+
+
+            points_new[points_new.length-1] = segments[idx_minRightEnd].end;
+            points = calcOptimalPoints(segments, points_new);
+            return points;
+        }
     }
 
     private static class Segment {
         int start, end;
+        boolean visited;
 
         Segment(int start, int end) {
             this.start = start;
             this.end = end;
+            this.visited = false;
         }
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
--- src/DotProduct.java
+++ src/DotProduct.java
@@ -4,11 +4,60 @@ public class DotProduct {
     private static long maxDotProduct(int[] a, int[] b) {
         //write your code here
         long result = 0;
-        for (int i = 0; i < a.length; i++) {
-            result += a[i] * b[i];
-        }
+//        for (int i = 0; i < a.length; i++) {
+//            result += a[i] * b[i];
+ //       }
+        result = calcMaxDotProduct(a, b, result);
+
+
         return result;
     }
+
+    public static long calcMaxDotProduct(int[] a, int[] b, long result){
+        if (a.length == 0) return result;
+
+        int max_a = -1000000;
+        int max_b = -1000000;
+        int idx_max_a = -1;
+        int idx_max_b = -1;
+
+        for (int i = 0; i < a.length; i++){
+            if (a[i] > max_a){
+                max_a = a[i];
+                idx_max_a = i;
+            }
+            if (b[i] > max_b){
+                max_b = b[i];
+                idx_max_b = i;
+            }
+        }
+        result = result + (long)max_a*(long)max_b;
+        int [] a_new = new int[a.length-1];
+        int [] b_new = new int[b.length-1];
+
+        for(int i=0; i < (a.length-1); i++){
+            if (i < idx_max_a){
+                a_new[i] = a[i];
+            }
+            else {
+                a_new[i]=a[i+1];
+            }
+        }
+
+        for(int i=0; i < (b.length-1); i++){
+            if (i < idx_max_b){
+                b_new[i] = b[i];
+            }
+            else {
+                b_new[i]=b[i+1];
+            }
+        }
+
+        result = calcMaxDotProduct(a_new, b_new, result);
+        return result;
+    }
+
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
