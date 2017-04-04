63299c2952a16a9e1f029830edf2ae2a2306dad4
Igor <zolotov.igor@gmail.com>
2017-04-04 23:14:46 +0300

    done Inversions from homework week 4

--- src/Inversions.java
+++ src/Inversions.java
@@ -2,22 +2,34 @@ import java.util.*;
 
 public class Inversions {
 
-    private static long getNumberOfInversions(int[] a) {
-        long numberOfInversions = 0;
-        int[] aSorted = mergeSort(a);
+    public static class SortedArray{
+        int[] sortedArray;
+        long inversionsCount;
 
-        for (int j = 0; j < aSorted.length; j++) {
-            System.out.print(aSorted[j] + " ");
+        public SortedArray(int[] sortedArray, long inversionsCount) {
+            this.sortedArray = sortedArray;
+            this.inversionsCount = inversionsCount;
         }
-        System.out.println("\n");
-
-        return numberOfInversions;
     }
 
-    private static int[] mergeSort(int[] a){
-        int left =0;
+    private static long getNumberOfInversions(int[] a) {
+        SortedArray b = mergeSort(a);
+
+//        for (int j = 0; j < b.sortedArray.length; j++) {
+//            System.out.print(b.sortedArray[j] + " ");
+//        }
+//        System.out.println("\n");
+
+        return b.inversionsCount;
+    }
+
+    private static SortedArray mergeSort(int[] a){
+        int left = 0;
         int right = a.length-1;
-        if(left>=right) return a;
+        if(left>=right){
+            SortedArray aSorted =  new SortedArray(a, 0);
+            return aSorted;
+        }
         else {
             int mid = (right+left)/2;
             int[] aLeft = new int[mid+1];
@@ -26,36 +38,40 @@ public class Inversions {
             int[] aRight = new int[right-mid];
             System.arraycopy(a, mid+1, aRight, 0, right-mid);
 
-            int[] aLeftSorted = mergeSort(aLeft);
-            int[] aRightSorted = mergeSort(aRight);
+            SortedArray aLeftSorted = mergeSort(aLeft);
+            SortedArray aRightSorted = mergeSort(aRight);
 
-            int[] aSorted = merge(aLeftSorted, aRightSorted);
+            SortedArray aSorted = merge(aLeftSorted, aRightSorted);
+            aSorted.inversionsCount = aSorted.inversionsCount + aLeftSorted.inversionsCount+aRightSorted.inversionsCount;
+
             return aSorted;
         }
     }
 
 
-    private static int[] merge(int[] aLeft, int[] aRight){
-        int[] aMerged = new int[aLeft.length+aRight.length];
+    private static SortedArray merge(SortedArray aLeft, SortedArray aRight){
+        SortedArray aMerged = new SortedArray(new int[aLeft.sortedArray.length+aRight.sortedArray.length], 0);
 
         int idx_left=0;
         int idx_right=0;
-        while (idx_left < aLeft.length || idx_right < aRight.length){
+        while (idx_left < aLeft.sortedArray.length || idx_right < aRight.sortedArray.length){
             int leftCur=0;
             int rightCur=0;
-            if(idx_left == aLeft.length) leftCur = 1000000001;
-            else leftCur = aLeft[idx_left];
+            if(idx_left == aLeft.sortedArray.length) leftCur = 1000000001;
+            else leftCur = aLeft.sortedArray[idx_left];
 
-            if (idx_right == aRight.length) rightCur = 1000000001;
-            else rightCur = aRight[idx_right];
+            if (idx_right == aRight.sortedArray.length) rightCur = 1000000001;
+            else rightCur = aRight.sortedArray[idx_right];
 
-            if (leftCur > rightCur){
-                aMerged[idx_left+idx_right] = rightCur;
-                idx_right++;
+            if (leftCur <= rightCur){
+                aMerged.sortedArray[idx_left+idx_right] = leftCur;
+                aMerged.inversionsCount = aMerged.inversionsCount+idx_right;
+                idx_left++;
+
             }
             else {
-                aMerged[idx_left+idx_right] = leftCur;
-                idx_left++;
+                aMerged.sortedArray[idx_left+idx_right] = rightCur;
+                idx_right++;
             }
 
         }
