77c50e647f03add4862c6b93de642d6e4cce0341
Igor <zolotov.igor@gmail.com>
2017-04-08 17:35:56 +0300

    add code for EditDistance

--- src/EditDistance.java
+++ src/EditDistance.java
@@ -2,8 +2,31 @@ import java.util.*;
 
 class EditDistance {
   public static int EditDistance(String s, String t) {
-    //write your code here
-    return 0;
+    int n = s.length()+1;
+    int m = t.length()+1;
+    int[][] D = new int[n][m];
+     for(int i = 0; i < n; i++) D[0][i] = i;
+     for (int i = 0; i < m;i++) D[i][0] = i;
+
+    int match = 0;
+    int dismatch = 0;
+    int insertion = 0;
+    int delition = 0;
+
+    for (int i = 1; i < m; i++){
+      for (int j=1; j < n; j++){
+        insertion = D[i][j-1]+1;
+        delition = D[i-1][j]+1;
+        match = D[i-1][j-1];
+        dismatch = D[i-1][j-1]+1;
+
+
+        if (s.charAt(i-1) == t.charAt(j-1)) D[i][j] = Math.min(insertion, Math.min(delition, match));
+        else D[i][j] = Math.min(insertion, Math.min(delition,dismatch));
+      }
+    }
+
+    return D[n][m];
   }
   public static void main(String args[]) {
     Scanner scan = new Scanner(System.in);
