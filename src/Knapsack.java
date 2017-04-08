7b71695d4f987eb368558b28f9e715a8b8918056
Igor <zolotov.igor@gmail.com>
2017-04-08 16:53:08 +0300

    done Knapsack from homework week 5

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -1,15 +1,35 @@
 import java.util.*;
 
 public class Knapsack {
-    static int optimalWeight(int W, int[] w) {
+
+    private static List<Integer> hashW = new ArrayList<Integer>();
+
+    static private int optimalWeight(int W, int[] w) {
         //write you code here
-        int result = 0;
-        for (int i = 0; i < w.length; i++) {
-          if (result + w[i] <= W) {
-            result += w[i];
-          }
+        if(hashW.get(W) != -1) return hashW.get(W);
+
+        if (w.length == 1){
+            if(w[0] <= W) return w[0];
+            else return 0;
         }
-        return result;
+        else {
+            int[] w_without_last = new int[w.length-1];
+            System.arraycopy(w, 0, w_without_last, 0, w.length-1);
+            if (w[w.length-1] <= W){
+                int whenUse_w_end = optimalWeight(W-w[w.length-1], w_without_last)+w[w.length-1];
+                int whenDontUse_w_end = optimalWeight(W, w_without_last);
+
+                hashW.set(W, Math.max(whenUse_w_end, whenDontUse_w_end));
+                return hashW.get(W);
+            }
+            else
+            {
+                hashW.set(W, optimalWeight(W, w_without_last));
+                return hashW.get(W);
+            }
+
+
+        }
     }
 
     public static void main(String[] args) {
@@ -21,7 +41,11 @@ public class Knapsack {
         for (int i = 0; i < n; i++) {
             w[i] = scanner.nextInt();
         }
+        for (int i = 0; i <= W;i++) hashW.add(-1);
+
         System.out.println(optimalWeight(W, w));
     }
+
+
 }
 
