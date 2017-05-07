f8cee1a52df34b0857fccc787b85c82394e257ae
Igor <zolotov.igor@gmail.com>
2017-05-07 23:08:29 +0300

    done JobQueue from homework week 3 course 2

--- src/JobQueue.java
+++ src/JobQueue.java
@@ -34,17 +34,17 @@ public class JobQueue {
         // TODO: replace this code with a faster algorithm.
         assignedWorker = new int[jobs.length];
         startTime = new long[jobs.length];
-        long[] nextFreeTime = new long[numWorkers];
+        workers[] Workers = new workers[numWorkers];
+        for (int i = 0; i < numWorkers; i++) {
+            Workers[i] = new workers(i, 0);
+        }
+
         for (int i = 0; i < jobs.length; i++) {
             int duration = jobs[i];
-            int bestWorker = 0;
-            for (int j = 0; j < numWorkers; ++j) {
-                if (nextFreeTime[j] < nextFreeTime[bestWorker])
-                    bestWorker = j;
-            }
-            assignedWorker[i] = bestWorker;
-            startTime[i] = nextFreeTime[bestWorker];
-            nextFreeTime[bestWorker] += duration;
+            assignedWorker[i] = Workers[0].id;
+            startTime[i] = Workers[0].nextFreeTime;
+            Workers[0].nextFreeTime += duration;
+            siftDown(Workers, 0);
         }
     }
 
@@ -55,6 +55,40 @@ public class JobQueue {
         assignJobs();
         writeResponse();
         out.close();
+    }
+
+    public class workers {
+        int id;
+        long nextFreeTime;
+
+        public workers(int id, long nextFreeTime) {
+            this.id = id;
+            this.nextFreeTime = nextFreeTime;
+        }
+    }
+
+    public void swapElements(workers[] Workers, int idx1, int idx2){
+        workers tmp=Workers[idx1];
+        Workers[idx1] = Workers[idx2];
+        Workers[idx2] = tmp;
+    }
+
+    public void siftDown(workers[] Workers, int i){
+        // for min-heap
+        int minIdx = i;
+        int l = 2*i+1; // leftchild
+        if (l<=Workers.length-1 && Workers[l].nextFreeTime == Workers[minIdx].nextFreeTime && Workers[l].id < Workers[minIdx].id) minIdx = l;
+        else if (l<=Workers.length-1 && Workers[l].nextFreeTime < Workers[minIdx].nextFreeTime) minIdx = l;
+
+
+        int r =2*i+2; //rightchild
+        if (r <= Workers.length-1 && Workers[r].nextFreeTime == Workers[minIdx].nextFreeTime && Workers[r].id < Workers[minIdx].id) minIdx = r;
+        else if (r <= Workers.length-1 && Workers[r].nextFreeTime < Workers[minIdx].nextFreeTime) minIdx = r;
+
+        if (i != minIdx){
+            swapElements(Workers, i, minIdx);
+            siftDown(Workers, minIdx);
+        }
     }
 
     static class FastScanner {
