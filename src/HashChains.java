9d3636a82796a9c285e71c7a661f79e3160ed37b
Igor <zolotov.igor@gmail.com>
2017-05-21 15:38:22 +0300

    done HashChains from week 4 course 2

--- src/HashChains.java
+++ src/HashChains.java
@@ -9,7 +9,7 @@ public class HashChains {
     private FastScanner in;
     private PrintWriter out;
     // store all strings in one list
-    private List<String> elems;
+    private List<String>[] elems;
     // for hash function
     private int bucketCount;
     private int prime = 1000000007;
@@ -46,19 +46,18 @@ public class HashChains {
     private void processQuery(Query query) {
         switch (query.type) {
             case "add":
-                if (!elems.contains(query.s))
-                    elems.add(0, query.s);
+                if (!elems[hashFunc(query.s)].contains(query.s))
+                    elems[hashFunc(query.s)].add(0, query.s);
                 break;
             case "del":
-                if (elems.contains(query.s))
-                    elems.remove(query.s);
+                if (elems[hashFunc(query.s)].contains(query.s))
+                    elems[hashFunc(query.s)].remove(query.s);
                 break;
             case "find":
-                writeSearchResult(elems.contains(query.s));
+                writeSearchResult(elems[hashFunc(query.s)].contains(query.s));
                 break;
             case "check":
-                for (String cur : elems)
-                    if (hashFunc(cur) == query.ind)
+                for (String cur : elems[query.ind])
                         out.print(cur + " ");
                 out.println();
                 // Uncomment the following if you want to play with the program interactively.
@@ -70,10 +69,11 @@ public class HashChains {
     }
 
     public void processQueries() throws IOException {
-        elems = new ArrayList<>();
         in = new FastScanner();
         out = new PrintWriter(new BufferedOutputStream(System.out));
         bucketCount = in.nextInt();
+        elems = new ArrayList[bucketCount];
+        for (int i=0; i < bucketCount; i++) elems[i] = new ArrayList<>();
         int queryCount = in.nextInt();
         for (int i = 0; i < queryCount; ++i) {
             processQuery(readQuery());
