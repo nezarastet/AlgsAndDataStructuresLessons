e81b126fde30f86764b788d5d909c2f7c9fb214d
Igor <zolotov.igor@gmail.com>
2017-06-13 15:06:28 +0300

    done ConnectedComponents from homework week 1 course 3

--- src/ConnectedComponents.java
+++ src/ConnectedComponents.java
@@ -2,10 +2,24 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ConnectedComponents {
-    private static int numberOfComponents(ArrayList<Integer>[] adj) {
+
+    private static int numberOfComponents(ArrayList<Integer>[] adj, boolean[] visited) {
         int result = 0;
-        //write your code here
+        for (int i = 0; i < adj.length; i++){
+            if (visited[i] == false){
+                result++;
+                explore(i, adj, visited);
+            }
+        }
         return result;
+    }
+
+    private static void explore (int x, ArrayList<Integer>[] adj, boolean[] visited) {
+        visited[x] = true;
+        for (int i = 0; i < adj[x].size(); i++) {
+            int w = adj[x].get(i);
+            if (!visited[w]) explore(w, adj, visited);
+        }
     }
 
     public static void main(String[] args) {
@@ -13,8 +27,10 @@ public class ConnectedComponents {
         int n = scanner.nextInt();
         int m = scanner.nextInt();
         ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+        boolean[] visited = new boolean[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<Integer>();
+            visited[i] = false;
         }
         for (int i = 0; i < m; i++) {
             int x, y;
@@ -23,7 +39,7 @@ public class ConnectedComponents {
             adj[x - 1].add(y - 1);
             adj[y - 1].add(x - 1);
         }
-        System.out.println(numberOfComponents(adj));
+        System.out.println(numberOfComponents(adj, visited));
     }
 }
 
